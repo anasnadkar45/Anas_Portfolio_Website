ff04414f3ab45e4f1afaa9f5b6b184a039d39034
anasnadkar45 <anasnadkar23@gmail.com>
2024-03-04 20:36:22 +0530

    Adding Code

--- src/components/shared/ContactMe.jsx
+++ src/components/shared/ContactMe.jsx
@@ -1,25 +1,38 @@
 import React from 'react'
 import { Button } from '../ui/button'
 import { BorderButton } from '../ui/moving-border'
+import { IoMailUnread } from "react-icons/io5";
+import { IoLogoWhatsapp } from "react-icons/io";
+import Title from './Title';
 
 const ContactMe = () => {
     return (
-        <div className='max-w-5xl mx-auto px-8 min-h-[400px] flex justify-center items-center rounded-3xl bg-gradient-to-br from-[#1A1A1A] to-[#000000]'>
-            <div>
-                <h1 className='text-center text-6xl'>Get In Touch</h1>
-                <p className='text-center'>I'm currently looking for opportunities. Whether its a
-                    Full-Time Job or a Freelance work. Send me a message, I'll try to get back to you as soon as possible.</p>
-                <div className='flex justify-center'>
-                    <Button className='bg-[#FFEE00] text-black text-lg font-semibold gap-1  group px-4'>Say Hello</Button>
+        <div className='flex-col items-center'>
+            <div className='w-full flex justify-center'>
+                <Title>Contact</Title>
+            </div>
+
+            <div className='max-w-5xl mx-auto mt-8 px-8 py-14 space-y-5 flex-col justify-center 
+        items-center rounded-3xl bg-gradient-to-br from-[#1A1A1A] to-90% to-[#0e0e0eac]'>
+                <div className='space-y-5'>
+                    <h1 className='text-center text-6xl font-bold text-[#FFEE00]'>Get In Touch</h1>
+                    <p className='text-center text-slate-300 max-w-xl mx-auto text-lg'>I'm currently looking for opportunities. Whether its a
+                        Full-Time Job or a Freelance work. Send me a message, I'll try to get back to you as soon as possible.</p>
+                </div>
+                <div className='flex justify-center gap-6'>
+                    <Button className='bg-[#1AD46F] text-black text-lg font-semibold gap-1 group px-4'>
+                        <IoMailUnread size='25px' /> Say Hello
+                    </Button>
                     <BorderButton
-                        className="bg-slate-900 text-white border-slate-800 rounded-md text-lg font-semibold"
+                        className="bg-slate-900 text-white border-slate-800 rounded-md gap-1 
+                    text-lg font-semibold"
                     >
-                        Say Hello
+                        <IoLogoWhatsapp size='25px' /> Say Hello
                     </BorderButton>
                 </div>
             </div>
-
         </div>
+
     )
 }
 
--- src/components/shared/Title.jsx
+++ src/components/shared/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Title = ({children}) => {
     return (
-        <div className='-rotate-2'>
+        <div className=' w-fit'>
             <h1 className='capitalize text-6xl font-bold'>{children}</h1>
             <div className='h-2 bg-[#1AD46F] w-48 text-center mb-1 rounded-md'></div>
             <div className='h-2 bg-[#FFEE00] w-48 text-center rounded-md translate-x-2'></div>
